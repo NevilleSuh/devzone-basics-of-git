7255577c809e7f6dbfdaee25b96642df03c3a250
NevilleSuh <suhneville7001@gmail.com>
2025-03-19 11:55:26 +0100

    Initial commit of folder

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+require 'db.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+
+    $stmt = $conn->prepare("SELECT * FROM users WHERE username = :username");
+    $stmt->bindParam(':username', $username);
+    $stmt->execute();
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if ($user && password_verify($password, $user['password'])) {
+        echo "Login successful!";
+    } else {
+        echo "Invalid username or password!";
+    }
+}
+?>
